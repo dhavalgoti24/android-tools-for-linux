a663d05f58ced688bdfacbf8614c8143910e4082
kiddlu <kidd.dawny.lu@gmail.com>
2017-11-17 07:11:02 +0000

    add local StringPrintf
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- storage/iotop/tasklist.cpp
+++ storage/iotop/tasklist.cpp
@@ -55,10 +55,61 @@ bool TaskList::Scan(std::map<pid_t, std::vector<pid_t>>& tgid_map) {
   });
 }
 
+static void StringAppendV(std::string* dst, const char* format, va_list ap) {
+  // First try with a small fixed size buffer
+  char space[1024];
+
+  // It's possible for methods that use a va_list to invalidate
+  // the data in it upon use.  The fix is to make a copy
+  // of the structure before using it and use that copy instead.
+  va_list backup_ap;
+  va_copy(backup_ap, ap);
+  
+  int result = vsnprintf(space, sizeof(space), format, backup_ap);
+  va_end(backup_ap);
+
+  if ((result >= 0) && (result < sizeof(space))) {
+    // It fit
+    dst->append(space, result);
+    return;
+  }
+  int length = sizeof(space);
+  while (true) {
+    if (result < 0) {
+      // Older behavior: just try doubling the buffer size
+      length *= 2;
+    } else {
+      // We need exactly "result+1" characters
+      length = result+1;
+    }
+    char* buf = new char[length];
+
+    // Restore the va_list before we use it again
+    va_copy(backup_ap, ap);
+    result = vsnprintf(buf, length, format, backup_ap);
+    va_end(backup_ap);
+
+    if ((result >= 0) && (result < length)) {
+      // It fit
+      dst->append(buf, result);
+      delete[] buf;
+      return;
+    }
+    delete[] buf;
+  }
+}
+
+static std::string StringPrintf(const char* format, ...) {
+  va_list ap;
+  va_start(ap, format);
+  std::string result;
+  StringAppendV(&result, format, ap);
+  va_end(ap);
+  return result;
+}
+
 bool TaskList::ScanPid(pid_t tgid, std::vector<pid_t>& pid_list) {
-  char pid_char[50];
-  sprintf(pid_char, "/proc/%d/task", tgid);
-  std::string filename(pid_char); 
+  std::string filename = StringPrintf("/proc/%d/task", tgid);
 
   return ScanPidsInDir(filename, [&pid_list](pid_t pid) {
     pid_list.push_back(pid);
